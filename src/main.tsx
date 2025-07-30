14bf38052e518d4d75637247f1972681e7cc6fe9
Yoshinori Kitamura <kdimension.system.cx@gmail.com>
2025-07-30 23:47:23 +0900

    [add]MUI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    mode: 'light', // ダークモードなども選択可能
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </StrictMode>,
-)
+  </ThemeProvider>
+);
