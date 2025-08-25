0bf6908c599322ba77c678842f39b0db46b2f32b
kitamurasystem <kdimension.system.cx@gmail.com>
2025-08-25 09:38:00 +0900

    [update]noEmit -> emitDecreationOnly

--- src/App.d.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import type { User } from "firebase/auth";
-export declare const ContextUserAccount: React.Context<{
-    userAccount: User;
-    setUserAccount: React.Dispatch<React.SetStateAction<User>>;
-}>;
-declare const App: React.FC;
-export default App;
--- src/List.d.ts
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-export declare const List: React.FC;
-export default List;
