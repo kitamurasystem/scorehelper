68527f88f13b51a85695279beb82004ad68470de
Yoshinori Kitamura <kdimension.system.cx@gmail.com>
2025-11-18 00:57:44 +0900

    [update]imageProcessor thumbnail

--- functions/lib/imageProcessor.d.ts
+++ functions/lib/imageProcessor.d.ts
@@ -1,6 +1,7 @@
 import { StorageObjectData } from 'firebase-functions/storage';
 export interface ProcessResult {
     newFilePath: string;
+    thumbnailPath: string;
     fullText: string;
 }
 export declare function processImage(object: StorageObjectData): Promise<ProcessResult>;
